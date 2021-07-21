9182d468e552575c6b01424f8fdbb103ceba3c4d
elcorreia <elcorreia17@gmail.com>
2021-07-20 23:14:16 -0400

    adicionando amazon s3 para armazenamento dos arquivos

--- src/modules/users/services/UpdateUserAvatarService.ts
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -5,6 +5,8 @@ import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
 import uploadConfig from '@config/upload';
 import fs from 'fs';
+import DiskStorageProvider from '@shared/providers/StorageProvider/DiskStorageProvider';
+import S3StorageProvider from '@shared/providers/StorageProvider/S3StorageProvider';
 
 interface IRequest {
   user_id: string;
@@ -14,6 +16,7 @@ interface IRequest {
 class UpdateUserAvatarService {
   public async execute({ user_id, avatarFileName }: IRequest): Promise<User> {
     const userRepository = getCustomRepository(UsersRepository);
+    const storageProvider = new DiskStorageProvider();
 
     const user = await userRepository.findById(user_id);
 
@@ -21,16 +24,24 @@ class UpdateUserAvatarService {
       throw new AppError('User not found.');
     }
 
-    if (user.avatar) {
-      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
-      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
-
-      if (userAvatarFileExists) {
-        await fs.promises.unlink(userAvatarFilePath);
+    if (uploadConfig.driver === 's3') {
+      const s3StorageProvider = new S3StorageProvider();
+      if (user.avatar) {
+        await s3StorageProvider.deleteFile(user.avatar);
       }
+
+      const fileName = await s3StorageProvider.saveFile(avatarFileName);
+    } else {
+      const storageProvider = new DiskStorageProvider();
+
+      if (user.avatar) {
+        await storageProvider.deleteFile(user.avatar);
+      }
+
+      const fileName = await storageProvider.saveFile(avatarFileName);
     }
 
-    user.avatar = avatarFileName;
+    user.avatar = fileName;
 
     await userRepository.save(user);
 
